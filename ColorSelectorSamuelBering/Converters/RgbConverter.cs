7dfb1c6928b09cbd54ec8f59b3b4bfd075dce6d3
Samuel Bering <samuelbering@hotmail.com>
2018-10-19 14:28:25 +0200

    fixed rgbconverter

---
+++ ColorSelectorSamuelBering/Converters/RgbConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorSelectorSamuelBering
+{
+    public class RgbConverter : IMultiValueConverter
+    {
+        #region IMultiValueConverter Members
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var r =(byte)values[0];
+            var g = (byte)values[1];
+            var b = (byte)values[2];
+
+            return Color.FromRgb(r, g, b);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
